ba720d544bcce050a18f852b330673946185dafd
danielCPay <danieles@claimpay.net>
2024-05-28 09:38:53 -0500

    update ImportClaims

--- modules/Documents/workflows/ImportClaims.php
+++ modules/Documents/workflows/ImportClaims.php
@@ -652,12 +652,14 @@ class ImportClaims
     public static function import_claims_from_excel(Vtiger_Record_Model $recordModel)
     {
         \App\Log::warning('ImportClaims::import_claims_from_excel F-' . memory_get_usage(false) . " T-" . memory_get_usage(true));;
-        var_dump("Income import_claims_from_excel");
-        exit();
+        
         $path = $recordModel->getFileDetails()['path'];
         $fn = $recordModel->get('filename');
         $at = $recordModel->getFileDetails()['attachmentsid'];
 
+        var_dump("Income import_claims_from_excel");
+        exit();
+        
         $pf = Vtiger_Record_Model::getInstanceById(\App\Request::_get('portfolio'));
         if (empty($pf)) {
             $recordModel->set('verification_warnings', "Nothing imported - the workflow must be called from Portfolios module");
